52b069d565141128e4b404a4c79007ad1062a97a
Eskel <emirbajnazarov44@gmail.com>
2022-03-07 16:04:40 +0600

    Initial commit

---
+++ app/src/main/java/it/kimoterru/josefina/MainActivity.kt
@@ -0,0 +1,11 @@
+package it.kimoterru.josefina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
